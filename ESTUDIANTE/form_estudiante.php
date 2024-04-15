1e2e2e898ccc8e654a734a5ffa11058fbd3b5be4
dany040518 <140460227+dany040518@users.noreply.github.com>
2024-04-15 17:01:50 -0500

    Add files via upload

---
+++ ESTUDIANTE/form_estudiante.php
@@ -0,0 +1,52 @@
+<?php
+include("conexion.php");
+ 
+$DocId=$_POST["docId"];
+$Nombre=$_POST["nombre"];
+$Apellido=$_POST["apellido"];
+$Genero=$_POST["genero"];
+$FechaDeNacimiento=$_POST["fechaDeNacimiento"];
+$Telefono=$_POST["telefono"];
+$EPS=$_POST["eps"];
+$RH=$_POST["rh"];
+$Direccion=$_POST["direccion"];
+
+$sql = "SELECT COUNT(*) AS total FROM ESTUDIANTE";
+$result = sqlsrv_query($conn,$sql);
+$row = sqlsrv_fetch($result);
+//Obtener el valor numerico
+$total = sqlsrv_get_field($result, 0); 
+
+// Generar el nuevo ID
+$nuevo_numero = $total + 1;
+$IdAcudiente = "EST".$nuevo_numero;
+
+//Generar Correo Institucional
+$CorreoInstitucional=$Nombre.$Apellido.'@ims.edu.co';
+
+//FECHA DE NACIMIENTO
+$fechaFormateada = date('Y-m-d H:i:s', strtotime($FechaDeContratacion));
+
+$sql2="SELECT * FROM ESTUDIANTE WHERE DOCUMENTO_DE_IDENTIDAD = '$DocId'";
+$result2 = sqlsrv_query($conn,$sql2);
+$row2 = sqlsrv_fetch($result2);
+
+if ($row2==0){
+    $query="INSERT INTO ACUDIENTE(ID_ACUDIENTE,DOCUMENTO_DE_IDENTIDAD,NOMBRE,APELLIDO,GENERO,CORREO,TELEFONO,DIRECCION) VALUES('$IdAcudiente','$DocId','$Nombre','$Apellido','$Genero','$Correo','$Telefono','$Direccion')";
+        $res=sqlsrv_prepare($conn,$query);
+ 
+        if (sqlsrv_execute($res)){
+            header("Location: /sql/ESTUDIANTE/form_estudiante.html");
+            exit();
+        }else{
+            if( ($errors = sqlsrv_errors() ) != null) {
+                foreach( $errors as $error ) {
+                    echo "ERROR AL INGRESAR LOS DATOS: " . $error['message'];
+                }
+            }
+        }
+    }else if ($row2==1){
+        header("Location: form_acudiente.html");
+        exit();
+    }
+?>
