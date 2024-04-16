b68f9e613c410511f818a25b7c40019edef0b6b1
dany040518 <140460227+dany040518@users.noreply.github.com>
2024-04-16 15:23:12 -0500

    Update form_estudiante.php

--- ESTUDIANTE/form_estudiante.php
+++ ESTUDIANTE/form_estudiante.php
@@ -35,7 +35,7 @@ $IdAcudiente = "ACU".$total2;
 $CorreoInstitucional=$Nombre.$Apellido.'@ims.edu.co';
 
 //FECHA DE NACIMIENTO
-$fechaFormateada = date('Y-m-d H:i:s', strtotime($FechaDeContratacion));
+$fechaFormateada = date('Y-m-d H:i:s', strtotime($FechaDeNacimiento));
 
 // asignarle un curso al estudiante
 $letra = ['A', 'B', 'C']; // Array con las letras posibles
@@ -43,13 +43,32 @@ $indice = array_rand($letra); // Obtener un índice aleatorio del array
 $letraAleatoria = $letra[$indice]; // Obtener la letra aleatoria
 $Curso = $Grado . ' ' . $letraAleatoria;
 
+//buscar id del curso
+$sql3 = "SELECT * FROM GRADO WHERE NOMBRE='$Curso'";
+$result3 = sqlsrv_query($conn, $sql3);
+if ($result3 === false) {
+    die(print_r(sqlsrv_errors(), true)); // Imprime errores SQL si los hay
+}
+$row3 = sqlsrv_fetch_array($result3, SQLSRV_FETCH_ASSOC);
+if ($row3 === false) {
+    echo "No se encontraron registros para el grado: $Curso";
+} else {
+    if (isset($row3['ID_GRADO'])) {
+        $IdGrado = $row3['ID_GRADO'];
+        echo "El ID del grado '$Curso' es: $IdGrado";
+    } else {
+        echo "No se encontró la columna ID_DEPARTAMENTO en el resultado";
+    }
+}
+
 $sql2="SELECT * FROM ESTUDIANTE WHERE DOCUMENTO_DE_IDENTIDAD = '$DocId'";
 $result2 = sqlsrv_query($conn,$sql2);
 $row2 = sqlsrv_fetch($result2);
 
 if ($row2==0){
-    $query="INSERT INTO ESTUDIANTE(ID_ESTUDIANTE,DOCUMENTO_DE_IDENTIDAD,NOMBRE,APELLIDO,GENERO,FECHA_DE_NACIMIENTO,ID_GRADO,CORREO_INSTITUCIONAL,TELEFONO,EPS,RH,DIRECCION,ID_ACUDIENTE) VALUES('$IdEstudiante','$DocId','$Nombre','$Apellido','$Genero','$fechaFormateada','$IdGrado','$CorreoInstitucional','$Telefono','$EPS','$RH''$Direccion','$IdAcudiente')";
-        $res=sqlsrv_prepare($conn,$query);
+    $query="INSERT INTO ESTUDIANTE(ID_ESTUDIANTE,DOCUMENTO_DE_IDENTIDAD,NOMBRE,APELLIDO,GENERO,FECHA_DE_NACIMIENTO,ID_GRADO,CORREO_INSTITUCIONAL,TELEFONO,EPS,RH,DIRECCION,ID_ACUDIENTE) VALUES('$IdEstudiante','$DocId','$Nombre','$Apellido','$Genero','$fechaFormateada','$IdGrado','$CorreoInstitucional','$Telefono','$EPS','$RH','$Direccion','$IdAcudiente')";
+        echo $query;
+    $res=sqlsrv_prepare($conn,$query);
  
         if (sqlsrv_execute($res)){
             header("Location: /sql/ADMINISTRATIVO/HISTORIAL_ACADEMICO/form_historial.html");
