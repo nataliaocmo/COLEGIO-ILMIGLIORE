acfa7ee01cca00acc7736c2b52b313e60f5fef26
nataliaocmo <nataliaocmo@unisabana.edu.co>
2024-04-16 13:53:16 -0500

    horario id autogenerado

--- HORARIO/form_horario.php
+++ HORARIO/form_horario.php
@@ -1,7 +1,6 @@
 <?php
 include("conexion.php");
 
-$idHorario = $_POST["idHorario"];
 $dia = $_POST["dia"];
 $horaInicio = $_POST["horaInicio"];
 $horaFin = $_POST["horaFin"];
@@ -9,7 +8,17 @@ $departamento = $_POST["departamento"];
 $materia = $_POST["materia"];
 $profesor = $_POST["profesor"];
 
-$sql = "INSERT INTO HORARIO (ID_HORARIO, DIA, HORA_INICIO, HORA_FIN, DEPARTAMENTO, MATERIA, PROFESOR) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$sql = "SELECT COUNT(*) AS total FROM HORARIO";
+$result = sqlsrv_query($conn,$sql);
+$row = sqlsrv_fetch($result);
+//Obtener el valor numerico
+$total = sqlsrv_get_field($result, 0); 
+
+// Generar el nuevo ID
+$nuevo_numero = $total + 1;
+$idHorario = "HOR".$nuevo_numero;
+
+$sql = "INSERT INTO HORARIO (ID_HORARIO, DIA, HORA_INICIO, HORA_FIN, DEPARTAMENTO, MATERIA, PROFESOR) VALUES ($idHorario, $dia, $horaInicio, $horaFin , $departamento, $materia, $profesor)";
 $params = array($idHorario, $dia, $horaInicio, $horaFin, $departamento, $materia, $profesor);
 $res = sqlsrv_query($conn, $sql, $params);
 
