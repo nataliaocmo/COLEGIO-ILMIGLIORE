3822fd9e5ddabe150318bf22c1553b16d4367bf5
nataliaocmo <nataliaocmo@unisabana.edu.co>
2024-04-16 14:29:21 -0500

    id pago matricula

--- ADMINISTRATIVO/PAGO_MATRICULA/form_pago_matricula.php
+++ ADMINISTRATIVO/PAGO_MATRICULA/form_pago_matricula.php
@@ -1,14 +1,23 @@
 <?php
 include("conexion.php");
 
-$idPagoMatricula = $_POST["idPagoMatricula"];
 $idEstudiante = $_POST["idEstudiante"];
 $monto = $_POST["monto"];
 $fecha = $_POST["fecha"];
 $metodoPago = $_POST["metodoPago"];
 $tipoPago = $_POST["tipoPago"];
 
-$sql = "INSERT INTO PAGO_MATRICULA (ID_PAGO_MATRICULA, ID_ESTUDIANTE, MONTO_PAGADO, FECHA_PAGO, METODO_PAGO, TIPO_PAGO) VALUES (?, ?, ?, ?, ?, ?)";
+$sql = "SELECT COUNT(*) AS total FROM ID_PAGO_MATRICULA";
+$result = sqlsrv_query($conn,$sql);
+$row = sqlsrv_fetch($result);
+//Obtener el valor numerico
+$total = sqlsrv_get_field($result, 0); 
+
+// Generar el nuevo ID
+$nuevo_numero = $total + 1;
+$IdPagoMatricula = "PM".$nuevo_numero;
+
+$sql = "INSERT INTO PAGO_MATRICULA (ID_PAGO_MATRICULA, ID_ESTUDIANTE, MONTO_PAGADO, FECHA_PAGO, METODO_PAGO, TIPO_PAGO) VALUES ($IdPagoMatricula, $idEstudiante , $monto, $fecha, $metodoPago, $tipoPago)";
 $params = array($idPagoMatricula, $idEstudiante, $monto, $fecha, $metodoPago, $tipoPago);
 $res = sqlsrv_query($conn, $sql, $params);
 
