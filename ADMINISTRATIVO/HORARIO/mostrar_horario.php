c031e038e1585b037e157568e2855f2df8ccbad1
dany040518 <dannasamo@unisabana.edu.co>
2024-05-27 17:09:17 -0500

    Ultimas modificaciones
    Aqui esta horario, los cambios de andres, y ya esta bonito

--- ADMINISTRATIVO/HORARIO/mostrar_horario.php
+++ ADMINISTRATIVO/HORARIO/mostrar_horario.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
 if (!isset($_SESSION['horario_generado'])) {
-    header("Location: form_horario.html");
+    header("Location: asignar_horario.php");
     exit();
 }
 
 $horarioGenerado = $_SESSION['horario_generado'];
+include("conexion.php");
 ?>
 
 <!DOCTYPE html>
@@ -31,29 +32,27 @@ $horarioGenerado = $_SESSION['horario_generado'];
             <tbody>
                 <?php foreach ($horarioGenerado as $horario): ?>
                     <?php
-                    include("conexion.php");
                     $query = "SELECT HU.DIA, HU.HORA_DE_INICIO, HU.HORA_DE_FIN, A.NOMBRE AS ASIGNATURA, P.NOMBRE AS PROFESOR
                               FROM HORARIO H
                               JOIN HORARIO_UNICO HU ON H.ID_HORARIO_UNICO = HU.ID_HORARIO_UNICO
                               JOIN RELACION_PROFESOR_ASIGNATURA RPA ON H.ID_RELACION_PROFESOR_ASIGNATURA = RPA.ID_RELACION_PROFESOR_ASIGNATURA
                               JOIN ASIGNATURA A ON RPA.ID_ASIGNATURA = A.ID_ASIGNATURA
                               JOIN PROFESOR P ON RPA.ID_PROFESOR = P.ID_PROFESOR
-                              WHERE H.ID_HORARIO = ?";
-                    $params = array($horario['ID_HORARIO']);
-                    $result = sqlsrv_query($conn, $query, $params);
+                              WHERE H.ID_HORARIO = '{$horario['ID_HORARIO']}'";
+                    $result = sqlsrv_query($conn, $query);
                     $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
                     ?>
                     <tr>
-                        <td><?php echo htmlspecialchars($row['DIA']); ?></td>
-                        <td><?php echo htmlspecialchars($row['HORA_DE_INICIO']); ?></td>
-                        <td><?php echo htmlspecialchars($row['HORA_DE_FIN']); ?></td>
-                        <td><?php echo htmlspecialchars($row['ASIGNATURA']); ?></td>
-                        <td><?php echo htmlspecialchars($row['PROFESOR']); ?></td>
+                        <td><?php echo $row['DIA']; ?></td>
+                        <td><?php echo $row['HORA_DE_INICIO']; ?></td>
+                        <td><?php echo $row['HORA_DE_FIN']; ?></td>
+                        <td><?php echo $row['ASIGNATURA']; ?></td>
+                        <td><?php echo $row['PROFESOR']; ?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
-        <a href="form_horario.html">Volver</a>
+        <button onclick="window.location.href='asignar_horario.php'">Volver</button>
     </div>
 </body>
 </html>
