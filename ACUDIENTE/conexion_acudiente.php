50ad282d41f00df18889335aaed919cd3a968b72
nataliaocmo <nataliaocmo@unisabana.edu.co>
2024-05-28 18:00:27 -0500

    TODO

---
+++ ACUDIENTE/conexion_acudiente.php
@@ -0,0 +1,16 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$serverName="DESKTOP-07UTCOR";
+$connectionInfo=array("Database"=>"Proyecto","UID"=>"Acudientes","PWD"=>"root");
+$conn=sqlsrv_connect($serverName,$connectionInfo);
+
+if($conn){
+    echo "Conexion establecida.<br/>";
+}else{
+    echo"Conexion no se pudo establecer.<br/>";
+    die(print_r(sqlsrv_errors(),true));
+}
+?>
