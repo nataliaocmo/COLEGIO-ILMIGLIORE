d0172297e01f46e451386003bb44515d92813fa0
dany040518 <dannasamo@unisabana.edu.co>
2024-05-28 15:03:13 -0500

    a

--- ADMINISTRATIVO/CARGUE_MASIVO/conexion.php
+++ ADMINISTRATIVO/CARGUE_MASIVO/conexion.php
@@ -2,11 +2,3 @@
 $serverName="DESKTOP-07UTCOR";
 $connectionInfo=array("Database"=>"Proyecto","UID"=>"Danna","PWD"=>"root");
 $conn=sqlsrv_connect($serverName,$connectionInfo);
-
-if($conn){
-	echo "Conexion establecida.<br/>";
-}else{
-	echo"Conexion no se pudo establecer.<br/>";
-	die(print_r(sqlsrv_errors(),true));
-}
-?>
