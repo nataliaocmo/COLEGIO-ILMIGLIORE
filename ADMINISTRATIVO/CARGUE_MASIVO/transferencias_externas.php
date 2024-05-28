aacf26c5e2daba351789391d980a9ddec1ae7a10
dany040518 <dannasamo@unisabana.edu.co>
2024-05-28 08:41:16 -0500

    Transferencias externas css
    le anadi el enrutamiento para el css

--- ADMINISTRATIVO/CARGUE_MASIVO/transferencias_externas.php
+++ ADMINISTRATIVO/CARGUE_MASIVO/transferencias_externas.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carga Masiva de Estudiantes y Acudientes</title>
+    <link rel="stylesheet" href="transferencias_externas.css">
 </head>
 <body>
     <div class="container">
